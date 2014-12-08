fe9659edeb4a409d87f983c9bc3e78fc94141d71
Rainer Poisel <rainer.poisel@gmail.com>
2014-12-08 08:50:10 +0100

    working with a real cache

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ api = Api(app)
 
 class PCF8574(object):
 
-    def __init__(self, bus_id, address, value=0x00):
+    def __init__(self, bus_id, address):
         super().__init__()
         self.__bus = SMBus(bus_id)
         self.__address = address
-        self.__value = 0
-        self.value = value
+        self.__value = self.__getRealValue()
 
     @property
     def value(self):
@@ -29,19 +28,52 @@ class PCF8574(object):
     def flipBits(self, changeBits):
         self.value ^= changeBits
 
+    def __getRealValue(self):
+        value = self.__bus.read_byte(self.__address)
+        return (~value) & 0xff
 
-class WriteByteI2C(Resource):
+
+i2c_io = {
+        0x01: {
+            0x20: PCF8574(0x01, 0x20)
+        }
+}
+
+
+class WritePcf8574(Resource):
+
     def get(self, bus_id, address, value):
         bus_id = int(bus_id, 16)
         address = int(address, 16)
         value = int(value, 16)
 
-        PCF8574(bus_id, address, value)
+        if bus_id in i2c_io and address in i2c_io[bus_id]:
+            pcf = i2c_io[bus_id][address]
+            pcf.value = value
 
-        return {'bus_id': bus_id,
-                'address': address,
-                'value': value
-                }
+            return {'bus_id': bus_id,
+                    'address': address,
+                    'value': value
+                    }
+        else:
+            return {'error': 'IO not found.'}
+
+
+class ReadPcf8574(Resource):
+
+    def get(self, bus_id, address):
+        bus_id = int(bus_id, 16)
+        address = int(address, 16)
+
+        if bus_id in i2c_io and address in i2c_io[bus_id]:
+            pcf = i2c_io[bus_id][address]
+
+            return {'bus_id': bus_id,
+                    'address': address,
+                    'value': pcf.value
+                    }
+        else:
+            return {'error': 'IO not found.'}
 
 
 class VirtualInput(object):
@@ -67,27 +99,29 @@ class WriteByteVirtual(Resource):
 
     outputs = {
         0x00: VirtualInput(
-            PCF8574(1, 0x20)
+            i2c_io[0x01][0x20]
         )
     }
 
     def get(self, address, value):
         address = int(address, 16)
         if address not in WriteByteVirtual.outputs:
-            return {}
+            return {'error': 'Virtual IO not found.'}
         vInput = WriteByteVirtual.outputs[address]
         value = int(value, 16)
 
         changing_states = (value & (~vInput.last_states & 0xff))
 
-        vInput.output.flipBits(changing_states)
+        if changing_states != 0x00:
+            vInput.output.flipBits(changing_states)
         vInput.last_states = value
 
-        return {'output': vInput.output.value
-                }
+        return {'output': vInput.output.value}
 
-api.add_resource(WriteByteI2C,
+api.add_resource(WritePcf8574,
                  '/physical/0x<string:bus_id>/0x<string:address>/0x<string:value>')
+api.add_resource(ReadPcf8574,
+                 '/physical/0x<string:bus_id>/0x<string:address>')
 api.add_resource(WriteByteVirtual,
                  '/virtual/0x<string:address>/0x<string:value>')
 
